e7b49cbe9a0db44c431926248a33f9a065c354fd
dark1922 <jvdematos004@gmail.com>
2022-08-06 19:36:43 -0300

    criando uma classe

---
+++ OrientacaoObjeto/Aula03/Carro.java
@@ -0,0 +1,41 @@
+package algaworks.OrientacaoObjeto.Aula03;
+
+public class Carro {
+
+    private String fabricante;
+    private String modelo;
+    private String cor;
+    int anoFabricacao;
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+}
---
+++ OrientacaoObjeto/Prefix.java
@@ -0,0 +1,13 @@
+package algaworks.OrientacaoObjeto;
+
+public class Prefix {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        for (int i = x; i > 0; i--) {
+            System.out.printf("oi %d%n", i);
+        }
+    }
+}
